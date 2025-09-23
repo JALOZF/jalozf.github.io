646a1ec1d7e0679cfc7a88021776e5f6132ea761
Puff <46506971+dchecketts@users.noreply.github.com>
2025-09-23 09:58:04 -0600

    Learning debugging and syntax

---
+++ WDD-131/sandbox/debugging.js
@@ -0,0 +1,9 @@
+const pi = 3.14; 
+let area = 0;
+function circleArea(radius) {
+    // const area = radius * pi
+    return area = radius * pi
+};
+
+area = circleArea(3);
+console.log('The area is: ' + area);
