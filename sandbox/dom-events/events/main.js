3d58021576d88a13951719258c6b447be2cf41af
Puff <46506971+dchecketts@users.noreply.github.com>
2025-09-25 19:28:46 -0600

    dom-events ToDo

--- sandbox/dom-events/events/main.js
+++ sandbox/dom-events/events/main.js
@@ -1,28 +1,67 @@
-const buttonElement = document.getElementById("submitButton");
+let tasks = [];
 
-function copyInput(event) {
-    const inputElement = document.getElementById("inputBox");
-    const outputElement = document.getElementById("output");
-    outputElement.innerHTML = inputElement.value;
-}
-buttonElement.addEventListener("click", copyInput);
-
-// modify the copyInput callback to receive the event object
-function copyInput(event) {
-    // take a look at the event!
-    console.log(event);
-    const inputElement = document.getElementById("inputBox");
-    const outputElement = document.getElementById("output");
-    outputElement.innerHTML = inputElement.value;
+function taskTemplate(task) {
+    return `
+    <li ${task.completed ? 'class="strike"' : ""}>
+      <p>${task.detail}</p>
+      <div>
+        <span data-action="delete">❎</span>
+        <span data-action="complete">✅</span>
+      </div>
+    </li>`
 }
 
-const log = document.querySelector("#log");
-
-document.addEventListener("keydown", logKey);
-
-function logKey(e) {
-    // how do we know which key was pressed?
-    console.log(e);
-    // checkout e.code, e.key, and e.keyCode
-    // what is the difference?
+function renderTasks(tasks) {
+    // get the list element from the DOM
+    const listElement = document.querySelector("#todoList");
+    listElement.innerHTML = "";
+    // loop through the tasks array. transform (map) each task object into the appropriate HTML to represent a to-do.
+    const html = tasks.map(taskTemplate).join("");
+    listElement.innerHTML = html;
 }
+
+function newTask() {
+    // get the value entered into the #todo input
+    const task = document.querySelector("#todo").value;
+    // add it to our arrays tasks
+    tasks.push({ detail: task, completed: false });
+    // render out the list
+    renderTasks(tasks);
+}
+
+function removeTask(taskElement) {
+    // Notice how we are using taskElement instead of document as our starting point?
+    // This will restrict our search to the element instead of searching the whole document.
+    tasks = tasks.filter(
+        (task) => task.detail != taskElement.querySelector('p').innerText
+    );
+    taskElement.remove();
+}
+
+function completeTask(taskElement) {
+    const taskIndex = tasks.findIndex(
+        (task) => task.detail === taskElement.querySelector('p').innerText
+    );
+    tasks[taskIndex].completed = tasks[taskIndex].completed ? false : true;
+    taskElement.classList.toggle("strike");
+    console.log(tasks);
+}
+
+function manageTasks(e) {
+    // did they click the delete or complete icon?
+    console.log(e.target);
+    const parent = e.target.closest("li");
+    if (e.target.dataset.action === "delete") {
+        removeTask(parent);
+    }
+    if (e.target.dataset.action === "complete") {
+        completeTask(parent);
+    }
+}
+
+// Add your event listeners here
+document.querySelector("#submitTask").addEventListener("click", newTask);
+document.querySelector("#todoList").addEventListener("click", manageTasks);
+
+// render  the initial list of tasks (if any) when the page loads
+renderTasks(tasks);
